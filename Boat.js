2ad25fc49e2c67ced666d1fc904662ac2e0e8104
PK15298 <85899531+PK15298@users.noreply.github.com>
2022-10-07 13:18:53 +0530

    Update Boat.js

--- Boat.js
+++ Boat.js
@@ -1,18 +1,27 @@
 class Boat {
   constructor(x, y, width, height, boatPos, boatAnimation) {
-  this.animation = boatAnimation
+  
+    this.animation = boatAnimation;
+    this.speed = 0.05;
     this.body = Bodies.rectangle(x, y, width, height);
     this.width = width;
     this.height = height;
+
     this.boatPosition = boatPos;
-    this.image = loadImage("./assets/boat.png");
+    this.isBroken = false;
 
     World.add(world, this.body);
   }
-animate(){
-  this.speed += 0.05
-}
+  animate() {
+    this.speed += 0.05;
+  }
+
   remove(index) {
+    this.animation = brokenBoatAnimation;
+    this.speed = 0.05;
+    this.width = 300;
+    this.height = 300;
+    this.isBroken = true;
     setTimeout(() => {
       Matter.World.remove(world, boats[index].body);
       delete boats[index];
@@ -22,7 +31,7 @@ animate(){
   display() {
     var angle = this.body.angle;
     var pos = this.body.position;
-    var index = floor(this.speed % this.animation.length)
+    var index = floor(this.speed % this.animation.length);
 
     push();
     translate(pos.x, pos.y);
